91ee06866c07310bb7fa7a9c20c4f0b23666f9b9
alesikivan <obcofficiall@gmail.com>
2022-05-23 12:43:18 +0300

    added server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist', 'angular-assignmentor')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../dist', 'angular-assignmentor', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
